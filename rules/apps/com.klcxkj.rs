b06ef25897c36ed3c96bda3a9edf773a3803e5ba
blackyau <blackyau426@gmail.com>
2018-10-08 00:51:46 +0800

    1 update & 7 new
    com.huawei.rcsserviceapplication 华为
     RCS 服务
    com.klcxkj.rs 校园热水
    com.msd.consumerChinese 默沙东诊疗中文大众版
    com.tplink.ipc TP-LINK 安防
    com.ximalaya.ting.android 喜马拉雅FM
    com.yong.school SC掌上大学
    com.zhihu.daily.android 知乎日报
    me.drakeet.puremosaic 纯纯打码

---
+++ rules/apps/com.klcxkj.rs
@@ -0,0 +1,12 @@
+{
+    "package": "com.klcxkj.rs",
+    "recommended": true,
+    "verified": false,
+    "description": {
+        "zh_CN": "会在 储存空间 留下大量垃圾",
+        "zh": "會在 儲存空間 留下大量垃圾"
+    },
+    "authors": [
+      "BlackYau"
+    ]
+  }
